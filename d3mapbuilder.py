5a2d4803603931d8cbc989e0eab3edb551ad221b
Nils Schneider <nils.schneider@gmail.com>
2012-05-12 14:57:25 +0200

    make attribute db private

--- d3mapbuilder.py
+++ d3mapbuilder.py
@@ -1,21 +1,19 @@
 import json
 
 class D3MapBuilder:
-  db = None
-
   def __init__(self, db):
-    self.db = db
+    self._db = db
 
   def build(self):
     output = dict()
 
     output['nodes'] = [{'group': x.group, 'name': x.name,
                         'macs': ', '.join(x.macs)
-                       } for x in self.db.get_nodes() if x.online]
+                       } for x in self._db.get_nodes() if x.online]
     output['links'] = [{'source': x.pair[0], 'target': x.pair[1],
                         'distance': x.distance,
                         'strength': x.strength
-                       } for x in self.db.get_links()]
+                       } for x in self._db.get_links()]
 
     return json.dumps(output)
 
