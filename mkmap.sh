2ef209c67ff7cee6191f64a7a65aca53b021aec9
Nils Schneider <nils.schneider@gmail.com>
2012-06-05 17:43:43 +0200

    make scripts executable

--- mkmap.sh
+++ mkmap.sh
@@ -5,8 +5,8 @@ DEST=$1
 
 GWS=`(/usr/sbin/batctl gwl -n | tail -n +2 | grep -v '^No' | sed 's/=>//' | awk '{ print $1 }') | while read a; do echo -n "-g $a "; done`
 
-/usr/sbin/batctl vd json | python "$(dirname "$0")"/bat2nodes.py -a "$(dirname "$0")"/aliases.json $GWS - > $DEST/nodes.json.new
-/usr/sbin/batctl vd json | python "$(dirname "$0")"/bat2geomap.py -a "$(dirname "$0")"/aliases.json - > $DEST/geomap.kml.new
+/usr/sbin/batctl vd json | "$(dirname "$0")"/bat2nodes.py -a "$(dirname "$0")"/aliases.json $GWS - > $DEST/nodes.json.new
+/usr/sbin/batctl vd json | "$(dirname "$0")"/bat2geomap.py -a "$(dirname "$0")"/aliases.json - > $DEST/geomap.kml.new
 
 mv $DEST/nodes.json.new $DEST/nodes.json
 mv $DEST/geomap.kml.new $DEST/geomap.kml
