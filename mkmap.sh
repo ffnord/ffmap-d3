5a33f6bd787866110c2be3e409620c12c9cca821
Matthias Schiffer <mschiffer@universe-factory.net>
2012-06-06 01:58:31 +0200

    Detect own gateway state

--- mkmap.sh
+++ mkmap.sh
@@ -3,10 +3,17 @@
 DEST=$1
 
 
-GWS=`(/usr/sbin/batctl gwl -n | tail -n +2 | grep -v '^No' | sed 's/=>//' | awk '{ print $1 }') | while read a; do echo -n "-g $a "; done`
+GWL=`batctl gwl -n`
 
-/usr/sbin/batctl vd json | "$(dirname "$0")"/bat2nodes.py -a "$(dirname "$0")"/aliases.json $GWS - > $DEST/nodes.json.new
-/usr/sbin/batctl vd json | "$(dirname "$0")"/bat2geomap.py -a "$(dirname "$0")"/aliases.json - > $DEST/geomap.kml.new
+SELF=`echo "$GWL" | head -n 1 | sed -r -e 's@^.*MainIF/MAC: [^/]+/([0-9a-f:]+).*$@\1@'`
+GWS=`(echo "$GWL" | tail -n +2 | grep -v '^No' | sed 's/=>//' | awk '{ print $1 }') | while read a; do echo -n "-g $a "; done`
+
+if [ `cat /sys/class/net/bat0/mesh/gw_mode` = server ]; then
+    GWS="$GWS -g $SELF"
+fi
+
+batctl vd json | "$(dirname "$0")"/bat2nodes.py -a "$(dirname "$0")"/aliases.json $GWS - > $DEST/nodes.json.new
+batctl vd json | "$(dirname "$0")"/bat2geomap.py -a "$(dirname "$0")"/aliases.json - > $DEST/geomap.kml.new
 
 mv $DEST/nodes.json.new $DEST/nodes.json
 mv $DEST/geomap.kml.new $DEST/geomap.kml
