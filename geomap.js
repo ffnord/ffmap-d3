14cccc215b05309b628f3f3817b73bab05681f02
Nils Schneider <nils.schneider@gmail.com>
2013-02-01 23:47:05 +0100

    geomap with labels

--- geomap.js
+++ geomap.js
@@ -129,13 +129,6 @@ function update(data) {
              .attr("x2", function (d) { return project(d.target.geo)[0] })
              .attr("y2", function (d) { return project(d.target.geo)[1] })
 
-             // XXX
-    if (map.getZoom() == map.getMaxZoom())
-      nodes_svg.selectAll(".label").style("visibility", "visible")
-    else
-      nodes_svg.selectAll(".label").style("visibility", "hidden")
-      nodes_svg.selectAll(".label").style("visibility", "visible")
-
     position_labels(nodes_svg)
   }
 }
@@ -191,16 +184,19 @@ function cluster_labels(labels, max_distance) {
       })
     })
 
-    if (min_distance > max_distance)
-      break
-
     var i = best[0], j = best[1]
     var d = labels[i]
     var o = labels[j]
 
+    var n = d.nodes.length + o.nodes.length
+
+    if (min_distance > max_distance * Math.pow(n, 4))
+      break
+
     d.x = (d.x * d.nodes.length + o.x * o.nodes.length) / (d.nodes.length + o.nodes.length)
     d.y = (d.y * d.nodes.length + o.y * o.nodes.length) / (d.nodes.length + o.nodes.length)
     d.nodes = d.nodes.concat(o.nodes)
+    d.cluster_distance =
 
     labels[j] = labels[labels.length - 1]
     labels.pop()
@@ -218,7 +214,7 @@ function position_labels(nodes) {
   nodes.each( function (d) {
     var xy = project(d.geo)
     var label = {
-      angle: 0.0, // rotated around center 
+      angle: 0.0, // rotated around center
       offset: 0,
       nodes: [d],
       x: xy[0], y: xy[1],
@@ -232,7 +228,8 @@ function position_labels(nodes) {
     occupied.push({ x: p[0] - POINT_SIZE/2, y: p[1] - POINT_SIZE/2, width: POINT_SIZE, height: POINT_SIZE })
   })
 
-  labels = cluster_labels(labels, 30)
+  var zoom = map.getZoom() / map.getMaxZoom()
+  labels = cluster_labels(labels, 3 * Math.pow(1/zoom, 4))
 
   /*
   labels.forEach( function (d) {
@@ -251,7 +248,7 @@ function position_labels(nodes) {
       }
     })
   })
-  
+
   labels = labels.filter(function (d) { return d != undefined })
 
   g.select(".labels").selectAll(".label").remove()
@@ -264,9 +261,30 @@ function position_labels(nodes) {
 
   label_nodes.append("line")
 
-  label_g = label_nodes.append("g")
+  label_g = label_nodes.append("g").attr("class", "textbox").append("g")
 
-  label_g.append("text")
+  label_g.append("g").each( function (d) {
+    var o = d3.select(this)
+
+    if (d.nodes.length < 4)
+      d.nodes.forEach( function (n, i) {
+        var name
+        if (n.name)
+          name = n.name
+        else
+          name = n.id
+
+        if (n.flags.online)
+          name += " (" + (n.clients.length - 1) + ")"
+
+        o.append("text").text(name).attr("y", -i * 15)
+      })
+    else {
+      var clients = d.nodes.filter(function (d) { return d.flags.online }).map(function (d) { return d.clients.length - 1 }).reduce(function (p, n) { return p + n })
+      o.append("text").text(d.nodes.length + " Knoten (" + clients + ")").attr("y", 0).style("font-weight", "bold")
+    }
+  })
+  /*
            .text(function(d, i) {
              var text = d.nodes.map(function (d) {
                if (d.name)
@@ -278,58 +296,69 @@ function position_labels(nodes) {
              })
              return d.nodes.length
            })
+           */
 
   labelTextWidth = function (e) {
-    return e.parentNode.querySelector("text").getBBox().width + 3
+    return e.parentNode.querySelector("g").getBBox().width + 10
   }
 
   labelTextHeight = function (e) {
-    return e.parentNode.querySelector("text").getBBox().height
+    return e.parentNode.querySelector("g").getBBox().height + 5
   }
 
-  label_g.insert("rect", "text")
-            .attr("y", function(d) { return labelTextHeight(this) / (-2)})
-            .attr("x", function(d) { return labelTextWidth(this) / (-2)})
+  label_g.insert("rect", "g")
+            .attr("y", function (d) { return -labelTextHeight(this) + 5})
+            .attr("x", -5)
             .attr("width", function(d) { return labelTextWidth(this)})
             .attr("height", function (d) { return labelTextHeight(this)})
+
+  label_g.each( function (d) {
+    var o = d3.select(this)
+    var r = o.select("rect")
+    var x = -r.attr("width")/2 + 5
+    var y = r.attr("height")/2 - 5
+    o.attr("transform", "translate(" + [x,y].join(",") + ")")
+  })
 
   label_nodes.each( function (d) {
     d.box = this.getBBox()
   })
 
-  var voronoi = d3.geom.voronoi(labels.map( function (d){ return [d.x, d.y] }))
+  if (labels.length > 1) {
+    var voronoi = d3.geom.voronoi(labels.map( function (d){ return [d.x, d.y] }))
 
-  var area_centers = voronoi.map( function (poly) {
-    var poly = d3.geom.polygon(poly)
-    return [poly.area(), poly.centroid()]
-  })
-
-  labels.forEach( function (d, i) {
-    var poly = voronoi[i]
-    var vectors = poly.map( function (p) {
-      return [p[0] - d.x, p[1] - d.y]
-    }).sort( function (b, a) {
-      return (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1])
+    var area_centers = voronoi.map( function (poly) {
+      var poly = d3.geom.polygon(poly)
+      return [poly.area(), poly.centroid()]
     })
 
-    var v = poly.map( function (p) {
-      return [p[0] - d.x, p[1] - d.y]
-    }).reduce( function (p, n) {
-      return [p[0] + n[0], p[1] + n[1]]
+    labels.forEach( function (d, i) {
+      var poly = voronoi[i]
+      var vectors = poly.map( function (p) {
+        return [p[0] - d.x, p[1] - d.y]
+      }).sort( function (b, a) {
+        return (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1])
+      })
+
+      var v = poly.map( function (p) {
+        return [p[0] - d.x, p[1] - d.y]
+      }).reduce( function (p, n) {
+        return [p[0] + n[0], p[1] + n[1]]
+      })
+
+      var v = vectors[0]//area_centers[i][1]
+      d.voronoi_area = area_centers[i][0]
+      d.voronoi_center = v
+      var dx = v[0]// - d.x
+      var dy = v[1]// - d.y
+      var abs = Math.sqrt(dx*dx + dy*dy)
+      d.angle = Math.atan2(dy/abs, dx/abs) / (2*Math.PI)
     })
 
-    var v = vectors[0]//area_centers[i][1]
-    d.voronoi_area = area_centers[i][0]
-    d.voronoi_center = v
-    var dx = v[0]// - d.x
-    var dy = v[1]// - d.y
-    var abs = Math.sqrt(dx*dx + dy*dy)
-    d.angle = Math.atan2(dy/abs, dx/abs) / (2*Math.PI)
-  })
-
-  labels = labels.sort( function (a, b) {
-    return a.voronoi_area - b.voronoi_area
-  })
+    labels = labels.sort( function (a, b) {
+      return (a.nodes.length * a.voronoi_area) - (b.nodes.length * b.voronoi_area)
+    })
+  }
 
   labels.forEach( function (d, i) {
     var box
@@ -341,7 +370,7 @@ function position_labels(nodes) {
       i--
 
       if (i == 0) {
-        d.angle += Math.random()*0.05
+        d.angle += Math.random()*0.02
         d.offset -= 25
 
         i = 30
@@ -361,7 +390,30 @@ function position_labels(nodes) {
     occupied.push(box)
   })
 
+  hulls = labels.filter( function (d) { return d.nodes.length > 1 })
+                .map( function (d) {
+                    return convexhull(d.nodes
+                      .map( function (n) { return project(n.geo) })
+                      .map( function (n) {
+                        var d = POINT_SIZE/2
+                        return  ([[-d, -d], [-d, d], [d, d], [d, -d]]).map( function (m) {
+                            return [n[0] + m[0], n[1] + m[1]]
+                          })
+                        }).reduce( function (p, n) {
+                          return p.concat(n)
+                        })
+                  )
+                })
+
   label_nodes.each(label_at_angle)
+
+  g.select(".labels").selectAll(".hulls").remove()
+
+  var path = g.select(".labels").append("g").attr("class", "hulls").selectAll("path")
+  path = path.data(hulls.map(function(d) { return "M" + d.join("L") + "Z"; }), String)
+  path.exit().remove()
+  path.enter().append("path").attr("d", String)
+  path.order()
 
     /*
   g.select("#voronoi").remove()
@@ -371,7 +423,9 @@ function position_labels(nodes) {
   path.exit().remove();
   path.enter().append("path").attr("class", function(d, i) { return "q" + (i % 9) + "-9"; }).attr("d", String);
   path.order();
+    */
 
+    /*
   g.select("#occupied").remove()
 
   var boxes = g.append("g").attr("id", "occupied").selectAll("rect").data(occupied)
@@ -382,6 +436,115 @@ function position_labels(nodes) {
                .attr("width", function (d) { return d.width })
                .attr("height", function (d) { return d.height })
                */
+}
+
+function isLeft(P0, P1, P2) {
+    return (P1[0] - P0[0])*(P2[1] - P0[1]) - (P2[0] - P0[0])*(P1[1] - P0[1])
+}
+
+function convexhull(points) {
+    var P = points.sort(function (a, b) {
+                    var t = a[0] - b[0]
+                    if (t==0)
+                      t = a[1] - b[1]
+
+                    return t
+                  })
+
+    var n = P.length
+    var H = []
+
+        // the output array H[] will be used as the stack
+    var bot = 0,
+    top = (-1); // indices for bottom and top of the stack
+    var i; // array scan index
+    // Get the indices of points with min x-coord and min|max y-coord
+    var minmin = 0,
+        minmax;
+
+    var xmin = P[0][0];
+    for (i = 1; i < n; i++) {
+        if (P[i][0] != xmin) {
+            break;
+        }
+    }
+
+    minmax = i - 1;
+    if (minmax == n - 1) { // degenerate case: all x-coords == xmin
+        H[++top] = P[minmin];
+        if (P[minmax][1] != P[minmin][1]) // a nontrivial segment
+            H[++top] = P[minmax];
+        H[++top] = P[minmin]; // add polygon endpoint
+        return H
+    }
+
+    // Get the indices of points with max x-coord and min|max y-coord
+    var maxmin, maxmax = n - 1;
+    var xmax = P[n - 1][0];
+    for (i = n - 2; i >= 0; i--) {
+        if (P[i][0] != xmax) {
+            break;
+        }
+    }
+    maxmin = i + 1;
+
+    // Compute the lower hull on the stack H
+    H[++top] = P[minmin]; // push minmin point onto stack
+    i = minmax;
+    while (++i <= maxmin) {
+        // the lower line joins P[minmin] with P[maxmin]
+        if (isLeft(P[minmin], P[maxmin], P[i]) >= 0 && i < maxmin) {
+            continue; // ignore P[i] above or on the lower line
+        }
+
+        while (top > 0) { // there are at least 2 points on the stack
+            // test if P[i] is left of the line at the stack top
+            if (isLeft(H[top - 1], H[top], P[i]) > 0) {
+                break; // P[i] is a new hull vertex
+            }
+            else {
+                top--; // pop top point off stack
+            }
+        }
+
+        H[++top] = P[i]; // push P[i] onto stack
+    }
+
+    // Next, compute the upper hull on the stack H above the bottom hull
+    if (maxmax != maxmin) { // if distinct xmax points
+        H[++top] = P[maxmax]; // push maxmax point onto stack
+    }
+
+    bot = top; // the bottom point of the upper hull stack
+    i = maxmin;
+    while (--i >= minmax) {
+        // the upper line joins P[maxmax] with P[minmax]
+        if (isLeft(P[maxmax], P[minmax], P[i]) >= 0 && i > minmax) {
+            continue; // ignore P[i] below or on the upper line
+        }
+
+        while (top > bot) { // at least 2 points on the upper stack
+            // test if P[i] is left of the line at the stack top
+            if (isLeft(H[top - 1], H[top], P[i]) > 0) {
+                break;  // P[i] is a new hull vertex
+            }
+            else {
+                top--; // pop top point off stack
+            }
+        }
+
+        if (P[i][0] == H[0][0] && P[i][1] == H[0][1]) {
+            return H
+        }
+
+        H[++top] = P[i]; // push P[i] onto stack
+    }
+
+    if (minmax != minmin) {
+        H[++top] = P[minmin]; // push joining endpoint onto stack
+    }
+
+    return H
 }
 
 function label_box(label) {
@@ -401,7 +564,7 @@ function label_box(label) {
 function label_at_angle(label) {
   var object = d3.select(this)
   var box = label_box(label)
-  object.select("g").attr("transform", "translate(" + box.x + "," + box.y + ")")
+  object.select(".textbox").attr("transform", "translate(" + box.x + "," + box.y + ")")
   object.select("line")
     .attr("x1", 0)
     .attr("y1", 0)
