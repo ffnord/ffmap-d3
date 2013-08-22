e5729b356996d03a48339a3b8fd065321f00ee13
georgiamoon <georgia.bullen@gmail.com>
2013-08-22 16:10:29 -0400

    quality scale colors adjusted

--- links.js
+++ links.js
@@ -1,11 +1,9 @@
 var linkcolor = {'default':
                   d3.scale.linear()
                   .domain([1, 1.25, 1.5])
-                  .range(["#0a3", "orange", "red"]),
+                  .range(["#6FFF05", "E8A00C", "FF0000"]),
                  'wifi':
                   d3.scale.linear()
                   .domain([1, 3, 10])
-                  .range(["#0a3", "orange", "red"]),
+                  .range(["#6FFF05", "E8A00C", "FF0000"]),
                 }
-
-
