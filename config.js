996ae5d498460027f328f61eacb76ab991664316
ruben <ruben.barkow@eclabs.de>
2013-12-27 23:38:42 +0100

    configfile added

---
+++ config.js
@@ -0,0 +1,11 @@
+// to apply any changes here, enter "make" in you terminal
+var ffmapConfig = {
+	url: 			"https://kiel.freifunk.net",
+	sitename: 		"kiel.freifunk.net",
+	// initial gravity, friction, of the graph at pageload
+	gravity:		0.35,
+	friction:		0.73,
+	theta:			0.8,
+	// dummy:
+	end_of_config:		1
+};
