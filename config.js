3470a99c5d78715754c19e32ee1ba081f72b954e
ruben <ruben.barkow@eclabs.de>
2014-01-05 14:47:23 +0100

    configfile added to adjust navigation sitelink and initial gravity, friction and theta to community-fitting values

---
+++ config.js
@@ -0,0 +1,14 @@
+/* after making any changes here enter "make" in your terminal to appy the changes */
+
+var ffmapConfig = {
+  // link to your main community site:
+  url:       "/",
+  
+  // visible link in the navigation:
+  sitename:  "luebeck.freifunk.net",
+  
+  // initial gravity, friction, of the graph at pageload:
+  gravity:   0.15,
+  friction:  0.73,
+  theta:     0.8
+};
