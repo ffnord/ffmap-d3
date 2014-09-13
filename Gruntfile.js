70abfce3766c49bdffd3a03b7886cd8e24efa9d6
Julian Bäume <julian@svg4all.de>
2014-09-13 11:56:41 +0200

    refactor: grunt configuration
    * split up configuration into modules
    * clean coding style

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,134 +1,14 @@
-'use strict';
+"use strict"
 
 module.exports = function (grunt) {
-    grunt.config.merge({
-        connect: {
-            server: {
-                options: {
-                    base: 'build/', //TODO: once grunt-contrib-connect 0.9 is released, set index file
-                    livereload: true
-                }
-            }
-        }
-    });
-    grunt.config.merge({
-        copy: {
-            html: {
-                options: {
-                    process: function (content) {
-                        return content
-                            .replace('#cityname#', grunt.config('local.cityname'))
-                            .replace('#sitename#', grunt.config('local.sitename'))
-                            .replace('#url#', grunt.config('local.url'));
-                    }
-                },
-                src: ['*.html'],
-                expand: true,
-                cwd: 'templates/',
-                dest: 'build/'
-            },
-            images: {
-                src: ['img/*.png', 'css/*.png'],
-                expand: true,
-                dest: 'build/'
-            },
-            css: {
-                src: ['css/*.css'],
-                expand: true,
-                dest: 'build/'
-            }
-        }
-    });
-    grunt.config.merge({
-        uglify: {
-            options: {
-                sourceMap: true,
-                sourceMapIncludeSources: true
-            },
-            geomap: {
-                src: [
-                    'config.js',
-                    'lib/loader.js',
-                    'bower_components/d3/d3.js',
-                    'lib/links.js',
-                    'bower_components/leaflet/dist/leaflet-src.js',
-                    'lib/geomap.js'
-                ],
-                dest: 'build/geomap.js'
-            },
-            graph: {
-                src: [
-                    'config.js',
-                    'lib/loader.js',
-                    'bower_components/d3/d3.js',
-                    'lib/links.js',
-                    'lib/pacman.js',
-                    'lib/graph.js'
-                ],
-                dest: 'build/graph.js'
-            },
-            list: {
-                src: [
-                    'config.js',
-                    'lib/loader.js',
-                    'bower_components/d3/d3.js',
-                    'lib/links.js',
-                    'bower_components/jquery/dist/jquery.js',
-                    'bower_components/jquery.tablesorter/js/jquery.tablesorter.js',
-                    'lib/list.js'
-                ],
-                dest: 'build/list.js'
-            }
-        }
-    });
-    grunt.config.merge({
-        watch: {
-            sources: {
-                options: {
-                    livereload: true
-                },
-                files: ['{css,img}/*.png', 'css/*.css', 'lib/*.js', 'templates/*.html'],
-                tasks: ['default']
-            },
-            config: {
-                options: {
-                    reload: true
-                },
-                files: ['Gruntfile.js'],
-                tasks: []
-            }
-        }
-    });
-    grunt.config.merge({
-        checkDependencies: {
-            options: {
-                install: true
-            },
-            bower: {
-                options: {
-                    packageManager: 'bower'
-                }
-            },
-            npm: {}
-        }
-    });
-    grunt.config.merge({
-        local: grunt.file.readJSON('config.json')
-    });
-    grunt.config.merge({
-        clean: {
-            build: ['build/', '!build/nodes.json', 'node_modules/grunt-newer/.cache']
-        }
-    });
+  grunt.config.merge({
+    local: grunt.file.readJSON("config.json")
+  })
 
-    grunt.loadNpmTasks('grunt-check-dependencies');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-newer');
+  grunt.loadTasks("tasks")
 
-    grunt.registerTask('default', ['checkDependencies', 'newer:copy', 'newer:uglify']);
-    grunt.registerTask('dev', ['default', 'connect:server', 'watch']);
-};
+  grunt.loadNpmTasks("grunt-newer")
+
+  grunt.registerTask("default", ["checkDependencies", "newer:copy", "newer:uglify"])
+  grunt.registerTask("dev", ["default", "connect:server", "watch"])
+}
---
+++ tasks/build.js
@@ -0,0 +1,76 @@
+"use strict"
+
+module.exports = function(grunt) {
+  grunt.config.merge({
+    copy: {
+      html: {
+        options: {
+          process: function (content) {
+            return content
+              .replace("#cityname#", grunt.config("local.cityname"))
+              .replace("#sitename#", grunt.config("local.sitename"))
+              .replace("#url#", grunt.config("local.url"))
+          }
+        },
+        src: ["*.html"],
+        expand: true,
+        cwd: "templates/",
+        dest: "build/"
+      },
+      images: {
+        src: ["img/*.png", "css/*.png"],
+        expand: true,
+        dest: "build/"
+      },
+      css: {
+        src: ["css/*.css"],
+        expand: true,
+        dest: "build/"
+      }
+    },
+    uglify: {
+      options: {
+        sourceMap: true,
+        sourceMapIncludeSources: true
+      },
+      geomap: {
+        src: [
+          "config.js",
+          "lib/loader.js",
+          "bower_components/d3/d3.js",
+          "lib/links.js",
+          "bower_components/leaflet/dist/leaflet-src.js",
+          "lib/html5slider.js",
+          "lib/geomap.js"
+        ],
+        dest: "build/geomap.js"
+      },
+      graph: {
+        src: [
+              "config.js",
+              "lib/loader.js",
+              "bower_components/d3/d3.js",
+              "lib/links.js",
+              "lib/pacman.js",
+              "lib/graph.js"
+        ],
+        dest: "build/graph.js"
+      },
+      list: {
+        src: [
+        "config.js",
+        "lib/loader.js",
+          "bower_components/d3/d3.js",
+          "lib/links.js",
+          "bower_components/jquery/dist/jquery.js",
+          "bower_components/jquery.tablesorter/js/jquery.tablesorter.js",
+            "lib/list.js"
+        ],
+        dest: "build/list.js"
+      }
+    }
+  })
+
+  grunt.loadNpmTasks("grunt-contrib-copy")
+  grunt.loadNpmTasks("grunt-contrib-uglify")
+}
