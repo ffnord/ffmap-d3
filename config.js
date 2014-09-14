15e9f5aae27c773768715f2c73ee61e796ef1687
Julian Bäume <julian@svg4all.de>
2014-09-14 15:22:20 +0200

    port all JS sources to requirejs
    everything is nice and modular, now.

--- config.js
+++ config.js
@@ -1,23 +1,25 @@
-//TODO: remove no-unused-vars config, once switched to modules
-/*eslint no-unused-vars: [2, {"vars": "local"}] */
+define("config", function () {
+  /* after making any changes here enter "grunt" in your terminal to apply the changes */
+  "use strict"
 
-/* after making any changes here enter "make" in your terminal to apply the changes */
+  var ffmapConfig = {
+    // link to your main community site:
+    url:       "/",
 
-var ffmapConfig = {
-  // link to your main community site:
-  url:       "/",
+    // visible link in the navigation:
+    sitename:  "gothamcity.freifunk.net",
 
-  // visible link in the navigation:
-  sitename:  "gothamcity.freifunk.net",
+    // initial gravity, friction, of the graph at pageload:
+    gravity:   0.05,
+    friction:  0.73,
+    theta:     0.8,
+    charge:    1.0,
+    distance:  1.0,
+    strength:  1.0,
 
-  // initial gravity, friction, of the graph at pageload:
-  gravity:   0.05,
-  friction:  0.73,
-  theta:     0.8,
-  charge:    1.0,
-  distance:  1.0,
-  strength:  1.0,
+    // path to the nodes.json
+    nodesJSON: "nodes.json"
+  }
 
-  // path to the nodes.json
-  nodesJSON: "nodes.json"
-}
+  return ffmapConfig
+})
